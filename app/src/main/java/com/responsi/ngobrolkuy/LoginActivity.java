57296dcf33d175d05b7a09bb7f37e656bfa32c6b
firmankurniajati <113574606+firmankurniajati@users.noreply.github.com>
2022-11-02 09:08:01 +0700

    Update LoginActivity.java

--- app/src/main/java/com/responsi/ngobrolkuy/LoginActivity.java
+++ app/src/main/java/com/responsi/ngobrolkuy/LoginActivity.java
@@ -2,13 +2,49 @@ package com.responsi.ngobrolkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView btnlogin;
+    private TextView btnlupapassword;
+    private TextView btnregist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnlogin = findViewById(R.id.login);
+        btnlupapassword = findViewById(R.id.lupapassword);
+        btnregist = findViewById(R.id.register);
+        getSupportActionBar().hide();
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(LoginActivity.this, ProfilePicActivity.class);
+                startActivity(pindah);
+            }
+        });
+
+        btnregist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(pindah);
+            }
+        });
+
+        btnlupapassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(LoginActivity.this, LupapasswordActivity.class);
+                startActivity(pindah);
+            }
+        });
+
     }
 }
