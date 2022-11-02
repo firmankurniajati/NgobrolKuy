aff98d4a413e7e3c0a9aa6e27254c1219e025450
firmankurniajati <113574606+firmankurniajati@users.noreply.github.com>
2022-11-02 09:11:03 +0700

    Update RegisterActivity.java

--- app/src/main/java/com/responsi/ngobrolkuy/RegisterActivity.java
+++ app/src/main/java/com/responsi/ngobrolkuy/RegisterActivity.java
@@ -2,13 +2,37 @@ package com.responsi.ngobrolkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+    private TextView regist;
+    private TextView punyaakun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+        regist = findViewById(R.id.register2);
+        punyaakun = findViewById(R.id.sudahpunyaakun);
+
+        regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(RegisterActivity.this, ProfilePicActivity.class);
+                startActivity(pindah);
+            }
+        });
+
+        punyaakun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(pindah);
+            }
+        });
     }
 }
