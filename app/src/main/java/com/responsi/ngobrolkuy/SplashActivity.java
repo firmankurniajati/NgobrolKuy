58992217cf0ffee2879ec189449cb661921784db
firmankurniajati <113574606+firmankurniajati@users.noreply.github.com>
2022-11-02 09:09:27 +0700

    Update SplashActivity.java

--- app/src/main/java/com/responsi/ngobrolkuy/SplashActivity.java
+++ app/src/main/java/com/responsi/ngobrolkuy/SplashActivity.java
@@ -1,14 +1,28 @@
 package com.responsi.ngobrolkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
-
+    //waktu detik
+    private int waktu_loading=3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent intent=new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },waktu_loading);
     }
 }
